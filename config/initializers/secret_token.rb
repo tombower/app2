5b18d4a423cf88a8656bc108856094e7d5b6b28a
sketchstruct <sketchstruct@gmail.com>
2012-10-11 21:03:57 +0100

    first commit2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = '2b3ac013d28d9356c515edb404d4ea776dd56a5c853c547673e8b3349e9c75c8be7800251b05fb9469fd9e22daf560d508507e0b62a5ae7808bf2e1581a8fa71'
